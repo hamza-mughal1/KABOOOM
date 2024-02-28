760fb84762b1e3facb12d6e0e8c57ae4f4e3f5c4
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2024-02-28 14:24:36 +0500

    added comments

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import json
 
+# initializing client site
 SERVER = '192.168.0.108'  
 PORT = 8080        
 ADDR = (SERVER, PORT)
@@ -11,36 +12,51 @@ DISCONNECT_MESSAGE = "DISCONNECT_TRUE"
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+# function for receiving data (JSON)
 def re_msg(client : socket.socket):
+    # lenght of message we gonna receive (for handling size dynamically)
     msg_lenght = client.recv(HEADER).decode(FORMAT)
 
+    # if message is None
     if msg_lenght:
         pass
     else:
         return None
     
+    # receiving message using msg_lenght and converting back to JSON
     msg_lenght = int(msg_lenght)
     msg = client.recv(msg_lenght).decode(FORMAT)
     msg = json.loads(msg)
 
+    # printing 'msg' from JSON we received
     print(f"[MESSAGE] (content = {msg['msg']})")
 
+
+# function for sending data (JSON)
 def send_message(client : socket.socket, msg : dict):
+    # Conveting JSON into string and then into bit code
     message = json.dumps(msg).encode(FORMAT)
+
+    # sending lenght of the data we are going to send
     msg_len = len(message)
     send_lenght = str(msg_len).encode(FORMAT)
     send_lenght += b' ' * (HEADER - len(send_lenght))
     client.send(send_lenght)
+
+    # sending actual data (JSON)
     client.send(message)
-    re_msg(client)
+    
 
-
-send_message(client, {"msg" : "hola amigo"})
-input()
-# send_message(client, "Hamza Mughal here")
-# input()
-# send_message(client, "bing chilling")
-# input()
-# send_message(client, "nigga")
-# input()
-send_message(client, {"msg" : DISCONNECT_MESSAGE})
+if __name__ == "__main__":
+    msgs = [
+        "hola amigo",
+            "Hamza Mughal here",
+            "bing chilling",
+            "nigga",
+            DISCONNECT_MESSAGE
+            ]
+    
+    for i in msgs:
+        send_message(client, {"msg" : i})
+        re_msg(client)
+        input()
